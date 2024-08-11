f4c23caec567dc7aa5c2e20a6614ba4e63cb7fa2
[hillsonkc100] <[hillsonkc100@gmail.com]>
2024-08-11 21:35:53 +0545

    strings

--- Day_3/4_strings.py
+++ Day_3/4_strings.py
@@ -4,3 +4,50 @@
 # Creating an empty string
 a = str()
 # str() is a built-in function to create string in python
+print(a) # ""
+
+a = ""
+print(a) # empty string
+
+a = ''
+print(a) # empty string
+
+a = """"""
+print(a) # empty string
+
+a = ''''''
+print(a) # empty string
+
+print(bool(a)) # False
+
+# Creating  non-empty strings
+a = "Hello"
+b = 'Hello world'
+c = """Hello world"""
+d = '''Hello world'''
+
+print(bool(d)) # True
+
+# Accessing string elements
+# Accessing string elements are similar to accessing list elements i.e. Indexing and Slicing
+
+# Indexing
+# Forward indexing starts from 0 and reverse from -1
+data = "Hello World. I am learning python"
+
+print(data[7]) # o
+print(data[0]) # H
+print(data[100]) # Error
+
+print(data[-1]) # n
+print(data[-10]) # I
+print(data[-100]) # Error
+
+# Slicing
+data = "Hello World. I am learning python"
+
+print(data[0:5]) # Hello
+print(data[7:15]) # World
+print(data[0:100]) # Hello World. I am learning python
+print(data[:5]) # Hello
+print(data[7:]) # World. I am learning python
